c01736ecd4ab3d68b2e658bc9caeeac554caaed5
Richard Weston <github@riweston.io>
2022-02-20 09:58:19 +0000

    refactor: Adjusted logic to handle creating request

--- client.go
+++ client.go
@@ -7,56 +7,51 @@ import (
 	"time"
 )
 
+const HostURL string = "https://api.acloud.guru/v1/"
+
+type Client struct {
+	BaseUrl    string
+	HTTPClient *http.Client
+	Auth       HeaderStruct
+}
+
 type HeaderStruct struct {
 	xApiKey     string `json:"x-api-key"`
 	xConsumerId string `json:"x-consumer-id"`
 }
 
-const HostURL string = "https://api.acloud.guru/v1/"
-
-type Client struct {
-	HostURL    string
-	HTTPClient *http.Client
-	Token      string
-	Auth       HeaderStruct
+func NewClient(xApiKey, xConsumerId *string) (*Client, error) {
+	return &Client{
+		HTTPClient: &http.Client{Timeout: 10 * time.Second},
+		BaseUrl:    HostURL,
+		Auth: HeaderStruct{
+			xApiKey:     *xApiKey,
+			xConsumerId: *xConsumerId,
+		},
+	}, nil
 }
 
-func NewClient(host, xApiKey, xConsumerId *string) (*Client, error) {
-	c := Client{
-		HTTPClient: &http.Client{Timeout: 10 * time.Second},
-		HostURL:    HostURL,
+func (c *Client) newRequest(requestMethod, requestPath string) (*http.Request, error) {
+	req, err := http.NewRequest(requestMethod, fmt.Sprintf("%s/%s", c.BaseUrl, requestPath), nil)
+	if err != nil {
+		return nil, err
 	}
 
-	if host != nil {
-		c.HostURL = *host
-	}
-
-	if xApiKey == nil || xConsumerId == nil {
-		return &c, nil
-	}
-
-	c.Auth = HeaderStruct{
-		xApiKey:     *xApiKey,
-		xConsumerId: *xConsumerId,
-	}
-
-	return &c, nil
+	return req, nil
 }
 
 func (c *Client) doRequest(req *http.Request) ([]byte, error) {
 	req.Header = http.Header{
-		"Host":          []string{c.HostURL},
 		"Content-Type":  []string{"application/json"},
 		"x-api-key":     []string{c.Auth.xApiKey},
 		"x-consumer-id": []string{c.Auth.xConsumerId},
 	}
-
 	res, err := c.HTTPClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
 
+	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
--- subscription.go
+++ subscription.go
@@ -3,11 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
 func (c *Client) GetSubscription() (*Subscription, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/subscription", c.HostURL), nil)
+	req, err := c.newRequest("GET", "subscription")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
